2374e008b870132f624350857c1127f683da4c2d
Miguel Angel <miguelangelpacompia@gmail.com>
2021-07-25 13:37:08 -0500

    second commit - fixed errors

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,27 @@
 // postcss.config.js
+
+// const purgecss = require('@fullhuman/postcss-purgecss')
+
+// module.exports = {
+//     plugins: {
+//       tailwindcss: {},
+//       autoprefixer: {},
+//       purgecss({
+//         content: ['./**/*.html']
+//       })
+//     }
+//   }
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
+  plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+      // purgecss({
+      //   content: ['./**/*.html']
+      // }),
+      // require('cssnano')({
+      //   preset: 'default',
+      //  }),
+    ]
   }
